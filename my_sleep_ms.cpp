d6ec5c4020e10fa8e9cb14a2b4e286d176ab5d1a
Sir-Branch <rmerello@itba.edu.ar>
2016-10-19 20:11:01 -0300

    Added Compile Error if unknown O.S.

--- my_sleep_ms.cpp
+++ my_sleep_ms.cpp
@@ -1,16 +1,20 @@
 #include "my_sleep_ms.h"
 
 #ifdef unix
-#include <unistd.h>
 
+#include <unistd.h>
 void my_sleep_ms(int sleepMs){
     usleep(sleepMs * 1000);   // usleep takes sleep time in us (1 millionth of a second microsecond)
 }
 
 #elif  defined _WIN32 || defined _WIN64
+
 #include <windows.h>
 void my_sleep_ms(int sleepMs){
     Sleep(sleepMs);
 }
+
+#else
+#error "Unknown O.S."
 #endif
 
